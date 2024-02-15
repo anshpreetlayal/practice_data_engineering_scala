cc009642f298ea81ce37d2eab729a7250decda92
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-02-14 19:03:18 -0500

    scala basics

--- Scala_Basics/syntax_and_structure.scala
+++ Scala_Basics/syntax_and_structure.scala
@@ -1,1 +1,14 @@
+// Arithmetic and Boolean Expressions
+val sum = 1 + 2 * 3  // Calculates the sum of 1 and 2 multiplied by 3
+val isEven = (4 % 2 == 0) && (5 % 2 == 0)  // Checks if both 4 and 5 are even numbers
+
+// Conditional Expressions (if-then-else)
+val result = if (sum > 5) "greater than 5" else "less than or equal to 5"  // Determines if sum is greater than 5
+
+// Functions with Recursion
+def factorial(n: Int): Int = {  // Calculates the factorial of a number
+  if (n <= 1) 1
+  else n * factorial(n - 1)
+}
+val fact5 = factorial(5)  // Computes the factorial of 5
 
