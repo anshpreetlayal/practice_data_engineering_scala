0ffd857d6c4446a6a1ab6489ff8ace17a42f3d8f
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-02-21 18:51:41 -0500

    added content

--- Scala_Basics/Collections_and_Interations.scala
+++ Scala_Basics/Collections_and_Interations.scala
@@ -19,3 +19,29 @@ object CollectionsIterationsExample extends App {
   println("\nArray:")
   numbersArray.foreach(println) // Iterating over an array
 }
+
+object CollectionsIterationsExample2 extends App {
+  // Lists
+  val list = List(1, 2, 3, 4, 5)
+  println("List:")
+  list.foreach(println) // Iterating over a list
+
+  // List constructors
+  val emptyList = Nil // Empty list
+  val filledList = 1 :: 2 :: 3 :: Nil // Constructing a list
+
+  // Right associativity
+  val rightAssociativeList = 1 :: 2 :: 3 :: Nil // Same as 1 :: (2 :: (3 :: Nil))
+
+  // List operations
+  val sum = list.sum // Sum of elements in the list
+  val reversed = list.reverse // Reversed list
+  val filtered = list.filter(_ % 2 == 0) // Filter even numbers
+
+  // List pattern matching
+  list match {
+    case head :: tail => println(s"Head: $head, Tail: $tail") // Matching head and tail
+    case Nil => println("Empty list") // Matching empty list
+  }
+
+}
