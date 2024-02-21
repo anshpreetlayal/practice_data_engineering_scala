a529faa603d6f2f30a5ee4dd7e3ff22dd2674b01
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-02-21 17:58:03 -0500

    added content

--- Object-Oriented-Scala/Companion_Objects.scala
+++ Object-Oriented-Scala/Companion_Objects.scala
@@ -6,5 +6,18 @@ object Person {
   def apply(name: String, age: Int): Person = new Person(name, age)
 
   // Static method
-  def defaultPerson(): Person = new Person("John Doe", 30)
+  def defaultPerson(): Person = new Person("John moore", 30)
+}
+
+object CompanionObjectsExample extends App {
+  // Using the factory method from the companion object
+  val person1 = Person("Alice", 25)
+  val person2 = Person.apply("Bob", 35)
+
+  // Using the static method from the companion object
+  val defaultPerson = Person.defaultPerson()
+
+  println(s"Person 1: ${person1.name}, ${person1.age}")
+  println(s"Person 2: ${person2.name}, ${person2.age}")
+  println(s"Default Person: ${defaultPerson.name}, ${defaultPerson.age}")
 }
