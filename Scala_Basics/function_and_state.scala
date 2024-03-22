9cdc9208a5a1b02ce4107691e3a6fd0456474e4e
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-03-22 19:03:51 -0400

    added code for functions and state

---
+++ Scala_Basics/function_and_state.scala
@@ -0,0 +1,45 @@
+// Encapsulation of State
+var count = 0
+val incrementAndGet: () => Int = () => {
+  count += 1
+  count
+}
+
+println(incrementAndGet()) // Output: 1
+println(incrementAndGet()) // Output: 2
+
+// Modification of State
+val adder: Int => Int = {
+  var sum = 0
+  (x: Int) => {
+    sum += x
+    sum
+  }
+}
+
+println(adder(1)) // Output: 1
+println(adder(2)) // Output: 3
+println(adder(3)) // Output: 6
+
+// Statefulness and Functional Programming
+class Counter {
+  private var count = 0
+
+  def increment(): Int = {
+    count += 1
+    count
+  }
+
+  def decrement(): Int = {
+    count -= 1
+    count
+  }
+
+  def getCount: Int = count
+}
+
+val counter = new Counter
+println(counter.increment()) // Output: 1
+println(counter.increment()) // Output: 2
+println(counter.decrement()) // Output: 1
+println(counter.getCount)    // Output: 1
