e3f9b40152a3ca4cdb267bd5ae72b29a00ed9285
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-03-22 19:05:42 -0400

    added example for state

--- Scala_Basics/function_and_state.scala
+++ Scala_Basics/function_and_state.scala
@@ -43,3 +43,49 @@ println(counter.increment()) // Output: 1
 println(counter.increment()) // Output: 2
 println(counter.decrement()) // Output: 1
 println(counter.getCount)    // Output: 1
+
+
+// stateful predictor
+val statefulPredicate: Int => Boolean = {
+  var threshold = 10
+  (x: Int) => {
+    if (x >= threshold) {
+      threshold = x
+      true
+    } else {
+      false
+    }
+  }
+}
+
+println(statefulPredicate(5)) // Output: false
+println(statefulPredicate(12)) // Output: true
+
+
+//managing state in class
+class CounterWithReset {
+  private var count = 0
+
+  def increment(): Int = {
+    count += 1
+    count
+  }
+
+  def decrement(): Int = {
+    count -= 1
+    count
+  }
+
+  def reset(): Unit = {
+    count = 0
+  }
+
+  def getCount: Int = count
+}
+
+val counter = new CounterWithReset
+println(counter.increment()) // Output: 1
+println(counter.increment()) // Output: 2
+println(counter.decrement()) // Output: 1
+counter.reset()
+println(counter.getCount) // Output: 0
