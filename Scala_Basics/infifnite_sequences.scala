48755a9764efaa9ebc58fbe7cf6852110a8cd9dd
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-03-26 13:12:31 -0400

    added example

--- Scala_Basics/infifnite_sequences.scala
+++ Scala_Basics/infifnite_sequences.scala
@@ -11,7 +11,6 @@ println(firstTen) // Output: LazyList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
 // Recursive Function for Infinite Sequences:
-
 // Generate the Fibonacci sequence indefinitely using recursion
 def fibonacci(a: BigInt, b: BigInt): LazyList[BigInt] = a #:: fibonacci(b, a + b)
 
@@ -21,3 +20,21 @@ val fibonacciSequence: LazyList[BigInt] = fibonacci(0, 1)
 val firstFifteen: Seq[BigInt] = fibonacciSequence.take(15)
 
 println(firstFifteen) // Output: LazyList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
+
+
+
+//Custom Lazy Evaluation:
+//You can implement custom lazy evaluation techniques using Lazy or thunk concepts. Here's a simple example:
+
+// Custom lazy evaluation using a thunk
+def lazySequence(start: Int): LazyList[Int] = {
+  lazy val rest: LazyList[Int] = lazySequence(start + 1)
+  start #:: rest
+}
+
+val customSequence: LazyList[Int] = lazySequence(0)
+
+// Take the first 5 elements from the custom lazy sequence
+val firstFive: Seq[Int] = customSequence.take(5)
+
+println(firstFive) // Output: LazyList(0, 1, 2, 3, 4)
